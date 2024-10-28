59b4d213b16e3f3c7a136768cf6e92ed2a1a363c
Kauã Santos <116742943+Bladekkkkkk@users.noreply.github.com>
2024-10-27 22:00:27 -0400

    ADD USURIOAS

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,18 @@ type ProdutoType = {
     descricao:string,
     imagem:string
 }
+type UsuarioType = {
+  id: number,
+  name: string,
+  email: string,
+  created_at: string,
+  updated_at: string
+}
+
 function App() {
   const [nome, setNome] = useState("")
   const [produtos, setProdutos] = useState<ProdutoType []>([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([])
   //useEffects(O que fazer, quando Fazer) []=> Hora do carregamento da página
   useEffect(() => {
     setNome("yurezin")
@@ -18,6 +27,12 @@ function App() {
       .then(resposta => resposta.json())
       .then(dados => setProdutos(dados))
     //Colocar em uma variável
+  }, [])
+
+  useEffect(() => {
+    fetch("https://one022a-marketplace-prkj.onrender.com/usuarios")
+      .then(resposta => resposta.json())
+      .then(dados => setUsuarios(dados))
   }, [])
 
   return (
@@ -38,7 +53,25 @@ function App() {
         }
 
       </div>
+
+
+      
+      <h2>Usuários</h2>
+      <div className="usuarios-container">
+        {usuarios.map(usuario => {
+          return (
+            <div key={usuario.id} className="usuario-item">
+              <h3>{usuario.name}</h3>
+              <p><strong>Email:</strong> {usuario.email}</p>
+              <p><strong>Data de Criação:</strong> {new Date(usuario.created_at).toLocaleString()}</p>
+              <p><strong>Última Atualização:</strong> {new Date(usuario.updated_at).toLocaleString()}</p>
+            </div>
+          )
+        })}
+      </div>
+
     </>
+
   )
 }
 
